33705a3c9696bd17137bb19b5475b09375bbd487
Killian Couty <killian.couty@outlook.com>
2019-04-01 12:50:45 +0200

    Ajout structure SContext

--- Strategie/src/strategie.cpp
+++ Strategie/src/strategie.cpp
@@ -6,9 +6,18 @@
 #pragma warning(disable:4996)	// disable _CRT_SECURE_NO_WARNINGS
 #endif
 
+
+struct SContext
+{
+	int id;
+	int nbPlayers;
+	SPlayerInfo* infos;
+};
+
 API_EXPORT void* InitGame(unsigned int id, unsigned int nbPlayer, const SMap *map, SPlayerInfo *info)
 {
 	std::cout << "InitGame" << std::endl;
+	SContext *ctx = new SContext();
 
 	strcpy(info->name, "Test de librairie");
 	strcpy(info->members[0], "COUTY Killian");
@@ -16,8 +25,11 @@ API_EXPORT void* InitGame(unsigned int id, unsigned int nbPlayer, const SMap *ma
 	strcpy(info->members[2], "GAUDUCHEAU Clément");
 	strcpy(info->members[3], "PEUZIAT Thomas");
 	
+	ctx->id = id;
+	ctx->nbPlayers = nbPlayer;
+	ctx->infos = info;
 
-	return(nullptr);
+	return(ctx);
 }
 
 API_EXPORT int PlayTurn(unsigned int gameTurn, void *ctx, const SGameState *state, STurn *turn)
