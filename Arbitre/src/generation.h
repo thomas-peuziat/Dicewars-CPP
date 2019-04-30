6bc077b8f2eb94a53704d7d109b7e5814ab646b0
Thomas PEUZIAT <17217655+thomas-peuziat@users.noreply.github.com>
2019-04-30 13:10:23 +0200

    Modification ligne colonne generation.cpp

--- Arbitre/src/generation.h
+++ Arbitre/src/generation.h
@@ -10,7 +10,7 @@
 
 typedef std::pair<int, int> Coordinates;
 typedef std::vector<std::vector<int>> Matrix;
-typedef std::map<int, std::set<Coordinates>> Map;
+typedef std::map<int, std::set<Coordinates>> MapTerritoire;
 
 unsigned int setNumberTerritories(unsigned int nb_players);
 std::set<Coordinates> odd_coordinates(const Coordinates &coord, int L, int C);
@@ -20,5 +20,5 @@ void displayMatrix(const int L, const int C, const Matrix &matrix);
 void rd_expand_cell(const std::set<Coordinates> & list);
 bool isEven(int a);
 bool isIsolated(const Matrix &mat, Coordinates coord, int L, int C);
-void afficherMap(const Map &m);
+void afficherMap(const MapTerritoire &m);
 void initialisationMap();
